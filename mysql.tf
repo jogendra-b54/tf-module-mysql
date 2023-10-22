4d8382964f8017d4efb9463dd7ce511a71de843b
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-22 11:55:21 +0530

    Auto push

--- mysql.tf
+++ mysql.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "mysql" {
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   username             = "admin1"
-  password             = "RoboShop@1"
+  password             = "RoboShop1"
   parameter_group_name = aws_db_parameter_group.mysql_pg.name
   skip_final_snapshot  = true
   db_subnet_group_name    = aws_db_subnet_group.mysql_subnet_group.name
