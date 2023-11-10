efd3d301f2c5911af90ad6a1f963b8c8efdb2893
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-10 12:31:19 +0530

    Auto push

--- data.tf
+++ data.tf
@@ -12,11 +12,11 @@
 
 
 
-data "aws_secretsmanager_secret" "secrets" {
-  name = "robot/secrets"
-}
+# data "aws_secretsmanager_secret" "secrets" {
+#   name = "robot/secrets"
+# }
 
 
-data "aws_secretsmanager_secret_version" "secret_version" {
-  secret_id     = data.aws_secretsmanager_secret.secrets.id 
-}
+# data "aws_secretsmanager_secret_version" "secret_version" {
+#   secret_id     = data.aws_secretsmanager_secret.secrets.id 
+# }
