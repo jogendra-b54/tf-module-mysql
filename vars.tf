ebfe4f5e261d96feced404198ac3514f78fb2751
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-10 14:01:46 +0530

    Auto push

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "ENV" {}
 
- #variable "MYSQL_PORT_NUMBER" {}
- #variable "MYSQL_STORAGE" {}
- #variable "MYSQL_ENGINE_VERSION" {}
- #variable "MYSQL_INSTANCE_TYPE" {}
+ variable "MYSQL_PORT_NUMBER" {}
+ variable "MYSQL_STORAGE" {}
+ variable "MYSQL_ENGINE_VERSION" {}
+ variable "MYSQL_INSTANCE_TYPE" {}
